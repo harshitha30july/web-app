baee6ae601d0b8ae9a043be8c3409dc77de0eec2
harshitha30july <82657326+harshitha30july@users.noreply.github.com>
2021-07-22 07:40:40 +0530

    Add files via upload

---
+++ lc_room.js
@@ -0,0 +1,14 @@
+
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyBjlcS-OlXXsIdLCc8g9JsIlpEa5vUHbCw",
+    authDomain: "letschat-web-app-75a7b.firebaseapp.com",
+    databaseURL: "https://letschat-web-app-75a7b-default-rtdb.firebaseio.com",
+    projectId: "letschat-web-app-75a7b",
+    storageBucket: "letschat-web-app-75a7b.appspot.com",
+    messagingSenderId: "163208081643",
+    appId: "1:163208081643:web:208ae351c12d451aea4eae"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
